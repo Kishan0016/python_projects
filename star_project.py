0b7ad7d6042939bfe9b8c87186a21337f864eeac
Kishan Yadav <kishanyadav5987@gmail.com>
2026-01-22 17:36:17 +0530

    Add files via upload

---
+++ star_project.py
@@ -0,0 +1,110 @@
+# 7 star pattern
+
+'''
+*
+**    n = 5 
+***
+****
+*****
+'''
+
+# for i in range(1,6):
+#     for j in range(1, i+1):
+#         print('*', end='')
+#     print()
+
+'''
+*****
+****
+***
+**
+*
+'''
+
+# for i in range(1, 6):
+#     for j in range(5, i-1, -1):
+#         print('*', end='')
+#     print()
+
+'''
+     *
+    **
+   ***
+  ****         
+ ***** 
+'''
+# for i in range(0, 5):
+#     for j in range(0, 5-i-1):
+#         print(end=' ')
+#     for j in range(0, i+1):
+#         print('*', end='')
+#     print()
+
+'''
+1
+12
+123
+1234
+12345
+'''
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print(j, end='')
+#     print()
+
+'''
+******
+******
+******
+******
+******
+'''
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print('*', end='')
+#     print()
+
+'''
+11111
+22222
+33333
+44444
+55555
+'''
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(i,end='')
+#     print()
+
+'''
+1
+22
+333
+4444
+55555
+'''
+# for i in range(1, 6):
+#     for i in range(1, i+1):
+#         print(i, end='')
+#     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
